d67fb4aedac3d30be6077b4c6d00d6f6ff0d4cf4
btx <bernardo.tolosa@gcash.com>
2023-02-12 21:07:16 +0800

    load configuration from envoy

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::f32::consts::E;
+
 use proxy_wasm::traits::*;
 use proxy_wasm::types::*;
 use log::info;
@@ -5,28 +7,62 @@ use json;
 
 proxy_wasm::main! {{
     proxy_wasm::set_log_level(LogLevel::Trace);
-    proxy_wasm::set_root_context(|_| -> Box<dyn RootContext> { Box::new(HttpBodyRoot) });
+    proxy_wasm::set_root_context(|_| -> Box<dyn RootContext> { Box::new(HttpBodyRoot {
+        configuration: PluginConfiguration { key: String::new() }
+    }) });
 }}
 
-struct HttpBodyRoot;
+struct HttpBodyRoot {
+    configuration: PluginConfiguration
+}
+
 impl Context for HttpBodyRoot {}
 
 impl RootContext for HttpBodyRoot {
+    fn on_configure(&mut self, _plugin_configuration_size: usize) -> bool {
+        if let Some(config_bytes) = self.get_plugin_configuration() {
+            let config_str = String::from_utf8(config_bytes).unwrap();     
+            let json_value =  match json::parse(&config_str) {
+                Ok(value) => value,
+                Err(err) => {
+                    log::error!("{}",err);
+                    json::parse("{}").unwrap()
+                }
+            };
+            let key = &json_value["key"];
+            self.configuration = PluginConfiguration {
+                key: key.to_string(),
+            };
+            info!("loaded configuration {:?}", self.configuration)
+        }
+        true
+    }
     fn get_type(&self) -> Option<ContextType> {
         Some(ContextType::HttpContext)
     }
 
     fn create_http_context(&self, context_id: u32) -> Option<Box<dyn HttpContext>> {
-        Some(Box::new(HttpBody { context_id, total_request_body_size: 0}))
+        Some(Box::new(HttpBody {
+            context_id,
+            total_request_body_size: 0,
+            configuration: self.configuration.clone()
+        }))
     }
 }
 
 struct HttpBody {
     context_id: u32,
     total_request_body_size: usize,
+    configuration: PluginConfiguration
 }
 
 impl Context for HttpBody {
+}
+
+impl HttpBody {
+    fn validate_payload(&mut self, json_value: &json::JsonValue) -> bool {
+        json_value.has_key("foo")
+    }
 }
 
 impl HttpContext for HttpBody {
@@ -62,6 +98,15 @@ impl HttpContext for HttpBody {
                     json::parse("{}").unwrap()
                 }
             };
+            if !self.validate_payload(&json_value) {
+                // If the validation fails, send the 403 response,
+                self.send_http_response(
+                    403,
+                    vec![("Powered-By", "proxy-wasm")],
+                    Some(b"Not allowed!\n"));
+                // and terminates this traffic.
+                return  Action::Pause;
+            }
             info!("request: {} {}",self.context_id, json_value);
         }
         Action::Continue
@@ -84,7 +129,6 @@ impl HttpContext for HttpBody {
             return Action::Pause;
         }
 
-        // Replace the message body if it contains the text "secret".
         // Since we returned "Pause" previuously, this will return the whole body.
         if let Some(body_bytes) = self.get_http_response_body(0, body_size) {
             let body_str = String::from_utf8(body_bytes).unwrap();
@@ -93,4 +137,12 @@ impl HttpContext for HttpBody {
         }
         Action::Continue
     }
+}
+
+// pluginConfiguration is a type to represent an example configuration for this wasm plugin.
+#[derive(Clone, Debug)]
+struct PluginConfiguration {
+	// Example configuration field.
+	// The plugin will validate if those fields exist in the json payload.
+    key: String
 }
